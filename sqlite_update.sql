314f8364f79afab61fd878f5c8064a2d7b97f672
textfield <textfield@876d2303-7a48-0410-9ec4-da127085e931>
2008-05-11 18:45:55 +0000

    Provide update commands for version 6.00.

--- sqlite_update.sql
+++ sqlite_update.sql
@@ -63,4 +63,65 @@ SELECT	 item_oid,
 FROM	 videogame_borrower;
 
 /* Release 6.00 */
-/* Please recreate the database. */
+
+CREATE TABLE member_tmp
+(
+	memberid	 VARCHAR(16) NOT NULL PRIMARY KEY DEFAULT 1,
+	membersince	 VARCHAR(32) NOT NULL,
+	dob		 VARCHAR(32) NOT NULL,
+	sex		 VARCHAR(8) NOT NULL DEFAULT 'Female',
+	first_name	 VARCHAR(128) NOT NULL,
+	middle_init	 VARCHAR(1),
+	last_name	 VARCHAR(128) NOT NULL,
+	telephone_num	 VARCHAR(32),
+	street		 VARCHAR(256) NOT NULL,
+	city		 VARCHAR(256) NOT NULL,
+	state_abbr	 VARCHAR(16) NOT NULL DEFAULT 'N/A',
+	zip		 VARCHAR(16) NOT NULL
+);
+
+INSERT INTO member_tmp (SELECT * FROM member);
+DROP TABLE member;
+ALTER TABLE member_tmp RENAME TO member;
+
+DROP VIEW cd_borrower_vw;
+DROP VIEW dvd_borrower_vw;
+DROP VIEW book_borrower_vw;
+DROP VIEW magazine_borrower_vw;
+DROP VIEW videogame_borrower_vw;
+
+CREATE TABLE item_borrower
+(
+	item_oid	 BIGINT NOT NULL,
+	memberid	 VARCHAR(16) NOT NULL,
+	reserved_date	 VARCHAR(32) NOT NULL,
+	duedate		 VARCHAR(32) NOT NULL,
+	myoid		 BIGSERIAL PRIMARY KEY,
+	copyid		 VARCHAR(64) NOT NULL,
+	copy_number	 INTEGER NOT NULL DEFAULT 1,
+	reserved_by	 VARCHAR(128) NOT NULL,
+	type		 VARCHAR(16) NOT NULL
+);
+
+CREATE VIEW item_borrower_vw AS
+SELECT	 item_oid,
+	 myoid,
+	 copyid,
+	 copy_number,
+	 reserved_date,
+	 duedate,
+	 type
+FROM	 item_borrower;
+
+INSERT INTO item_borrower SELECT item_oid, memberid, reserved_date, duedate, myoid, copyid, copy_number, reserved_date, 'CD' FROM cd_borrower;
+INSERT INTO item_borrower SELECT item_oid, memberid, reserved_date, duedate, myoid, copyid, copy_number, reserved_date, 'DVD' FROM dvd_borrower;
+INSERT INTO item_borrower SELECT item_oid, memberid, reserved_date, duedate, myoid, copyid, copy_number, reserved_date, 'Book' FROM book_borrower;
+INSERT INTO item_borrower SELECT item_oid, memberid, reserved_date, duedate, myoid, copyid, copy_number, reserved_date, 'Journal' FROM magazine_borrower WHERE item_oid IN (SELECT myoid FROM magazine WHERE type = 'Journal');
+INSERT INTO item_borrower SELECT item_oid, memberid, reserved_date, duedate, myoid, copyid, copy_number, reserved_date, 'Magazine' FROM magazine_borrower WHERE item_oid IN (SELECT myoid FROM magazine WHERE type = 'Magazine');
+INSERT INTO item_borrower SELECT item_oid, memberid, reserved_date, duedate, myoid, copyid, copy_number, reserved_date, 'Video Game' FROM videogame_borrower;
+
+DROP TABLE cd_borrower;
+DROP TABLE dvd_borrower;
+DROP TABLE book_borrower;
+DROP TABLE magazine_borrower;
+DROP TABLE videogame_borrower;
