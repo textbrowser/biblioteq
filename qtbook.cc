2dcd7491515ef4e9f1a804f101828e8b205585b3
textfield <textfield@876d2303-7a48-0410-9ec4-da127085e931>
2008-08-27 11:44:34 +0000

    Version 6.09 changes.

--- qtbook.cc
+++ qtbook.cc
@@ -724,7 +724,7 @@ void qtbook::slotAbout(void)
   mb.setFont(qapp->font());
   mb.setWindowTitle("BiblioteQ: About");
   mb.setTextFormat(Qt::RichText);
-  mb.setText("<html>BiblioteQ Version 6.08.<br>"
+  mb.setText("<html>BiblioteQ Version 6.09.<br>"
 	     "Copyright (c) 2006, 2007, 2008 "
 	     "Slurpy McNash.<br>"
 	     "Icons copyright (c) Everaldo.<br><br>"
