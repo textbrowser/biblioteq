bb1d4627eb4ecf2b4d9677d9171085d121a5d94d
textfield <textfield@876d2303-7a48-0410-9ec4-da127085e931>
2009-08-17 12:39:34 +0000

    Selectable Z39.50 server.

---
+++ include.d/z3950results.h
@@ -0,0 +1,45 @@
+#ifndef _Z3950RESULTS_H_
+#define _Z3950RESULTS_H_
+
+/*
+** -- Qt Includes --
+*/
+
+#include <QDialog>
+#include <QWidget>
+#include <QStringList>
+
+/*
+** -- Local Includes --
+*/
+
+#include "ui_z3950results.h"
+#include "qtbook_magazine.h"
+
+class qtbook_magazine;
+
+class z3950results: QDialog
+{
+  Q_OBJECT
+
+ public:
+  z3950results(QWidget *, QStringList &, qtbook_magazine *, const QFont &);
+  ~z3950results();
+
+ private:
+  QStringList records;
+  qtbook_magazine *magazine;
+  Ui_z3950ResultsDialog ui;
+  void keyPressEvent(QKeyEvent *event);
+  void setGlobalFonts(const QFont &);
+
+ protected:
+  void closeEvent(QCloseEvent *);
+
+ private slots:
+  void slotClose(void);
+  void slotSelectRecord(void);
+  void slotUpdateQueryText(void);
+};
+
+#endif
---
+++ z3950results.cc
@@ -0,0 +1,110 @@
+#include "z3950results.h"
+
+/*
+** -- z3950results() --
+*/
+
+z3950results::z3950results(QWidget *parent, QStringList &list,
+			   qtbook_magazine *magazine_arg,
+			   const QFont &font):
+  QDialog(parent)
+{
+  int i = 0;
+
+  magazine = magazine_arg;
+  setWindowModality(Qt::WindowModal);
+  ui.setupUi(this);
+
+  for(i = 0; i < list.count(); i++)
+    {
+      ui.list->addItem(QString(tr("Record #")) + QString::number(i + 1));
+      records.append(list[i]);
+    }
+
+  list.clear();
+  connect(ui.list, SIGNAL(currentRowChanged(int)), this,
+	  SLOT(slotUpdateQueryText(void)));
+  connect(ui.okButton, SIGNAL(clicked(void)), this,
+	  SLOT(slotSelectRecord(void)));
+  connect(ui.cancelButton, SIGNAL(clicked(void)), this,
+	  SLOT(slotClose(void)));
+  ui.list->setCurrentRow(0);
+  ui.splitter->setStretchFactor(ui.splitter->indexOf(ui.recRet),  0);
+  ui.splitter->setStretchFactor(ui.splitter->indexOf(ui.recCont),  1);
+  setGlobalFonts(font);
+  exec();
+}
+
+/*
+** -- ~z3950results() --
+*/
+
+z3950results::~z3950results()
+{
+  records.clear();
+}
+
+/*
+** -- slotSelectRecord() --
+*/
+
+void z3950results::slotSelectRecord(void)
+{
+  QStringList list;
+
+  list = ui.textarea->toPlainText().split("\n");
+  close();
+  magazine->populateDisplayAfterZ3950(list);
+  list.clear();
+  deleteLater();
+}
+
+/*
+** -- slotUpdateQueryText() --
+*/
+
+void z3950results::slotUpdateQueryText(void)
+{
+  ui.textarea->setPlainText(records[ui.list->currentRow()]);
+}
+
+/*
+** -- closeEvent() --
+*/
+
+void z3950results::closeEvent(QCloseEvent *e)
+{
+  (void) e;
+  slotClose();
+}
+
+/*
+** -- slotClose() --
+*/
+
+void z3950results::slotClose(void)
+{
+  deleteLater();
+}
+
+/*
+** -- setGlobalFonts() --
+*/
+
+void z3950results::setGlobalFonts(const QFont &font)
+{
+  setFont(font);
+
+  foreach(QWidget *widget, findChildren<QWidget *>())
+    widget->setFont(font);
+}
+
+/*
+** -- keyPressEvent() --
+*/
+
+void z3950results::keyPressEvent(QKeyEvent *event)
+{
+  if(event->key() == Qt::Key_Escape)
+    slotClose();
+}
