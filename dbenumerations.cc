350d91fbd785a57c20cfb898164f0f4b74b8ac5d
textfield <textfield@876d2303-7a48-0410-9ec4-da127085e931>
2010-11-29 21:51:55 +0000

    Completed CD Formats and DVD Aspect Ratios.

--- dbenumerations.cc
+++ dbenumerations.cc
@@ -35,6 +35,14 @@ dbenumerations::dbenumerations(void):QMainWindow()
 	  SIGNAL(clicked(void)),
 	  this,
 	  SLOT(slotRemove(void)));
+  connect(ui.addDvdAspectRatio,
+	  SIGNAL(clicked(void)),
+	  this,
+	  SLOT(slotAdd(void)));
+  connect(ui.removeDvdAspectRatio,
+	  SIGNAL(clicked(void)),
+	  this,
+	  SLOT(slotRemove(void)));
 }
 
 /*
@@ -67,35 +75,52 @@ void dbenumerations::slotClose(void)
 
 void dbenumerations::populateWidgets(void)
 {
-  ui.cdFormatsList->clear();
-
   QString errorstr("");
   QStringList list;
 
-  qapp->setOverrideCursor(Qt::WaitCursor);
-  list = misc_functions::getCDFormats(qmain->getDB(),
-				      errorstr);
-  qapp->restoreOverrideCursor();
+  for(int i = 0; i < 10; i++)
+    {
+      QListWidget *listwidget = 0;
 
-  if(!errorstr.isEmpty())
-    qmain->addError
-      (QString(tr("Database Error")),
-       QString(tr("Unable to retrieve the cd formats.")),
-       errorstr, __FILE__, __LINE__);
-  else
-    while(!list.isEmpty())
-      {
-	QListWidgetItem *item = 0;
+      if(i == 0)
+	listwidget = ui.cdFormatsList;
+      else if(i == 1)
+	listwidget = ui.dvdAspectRatiosList;
 
-	item = new(std::nothrow) QListWidgetItem(list.takeFirst());
+      if(listwidget)
+	listwidget->clear();
 
-	if(item)
+      qapp->setOverrideCursor(Qt::WaitCursor);
+
+      if(i == 0)
+	list = misc_functions::getCDFormats(qmain->getDB(),
+					    errorstr);
+      else if(i == 1)
+	list = misc_functions::getDVDAspectRatios(qmain->getDB(),
+						  errorstr);
+
+      qapp->restoreOverrideCursor();
+
+      if(!errorstr.isEmpty())
+	qmain->addError
+	  (QString(tr("Database Error")),
+	   QString(tr("Unable to retrieve the cd formats.")),
+	   errorstr, __FILE__, __LINE__);
+      else if(listwidget)
+	while(!list.isEmpty())
 	  {
-	    item->setFlags(Qt::ItemIsEditable | Qt::ItemIsEnabled |
-			   Qt::ItemIsSelectable) ;
-	    ui.cdFormatsList->addItem(item);
+	    QListWidgetItem *item = 0;
+
+	    item = new(std::nothrow) QListWidgetItem(list.takeFirst());
+
+	    if(item)
+	      {
+		item->setFlags(Qt::ItemIsEditable | Qt::ItemIsEnabled |
+			       Qt::ItemIsSelectable) ;
+		listwidget->addItem(item);
+	      }
 	  }
-      }
+    }
 }
 
 /*
@@ -115,18 +140,25 @@ void dbenumerations::slotAdd(void)
 {
   QToolButton *toolButton = qobject_cast<QToolButton *>
     (sender());
+  QListWidget *list = 0;
   QListWidgetItem *listItem = 0;
 
   if(toolButton == ui.addCdFormat)
     {
+      list = ui.cdFormatsList;
       listItem = new(std::nothrow) QListWidgetItem(tr("CD Format"));
+    }
+  else if(toolButton == ui.addDvdAspectRatio)
+    {
+      list = ui.dvdAspectRatiosList;
+      listItem = new(std::nothrow) QListWidgetItem(tr("DVD Aspect Ratio"));
+    }
 
-      if(listItem)
-	{
-	  listItem->setFlags(Qt::ItemIsEditable | Qt::ItemIsEnabled |
-			     Qt::ItemIsSelectable) ;
-	  ui.cdFormatsList->addItem(listItem);
-	}
+  if(list && listItem)
+    {
+      listItem->setFlags(Qt::ItemIsEditable | Qt::ItemIsEnabled |
+			 Qt::ItemIsSelectable) ;
+      list->addItem(listItem);
     }
 }
 
@@ -136,11 +168,17 @@ void dbenumerations::slotAdd(void)
 
 void dbenumerations::slotRemove(void)
 {
+  QListWidget *list = 0;
   QToolButton *toolButton = qobject_cast<QToolButton *>
     (sender());
 
   if(toolButton == ui.removeCdFormat)
-    delete ui.cdFormatsList->takeItem(ui.cdFormatsList->currentRow());
+    list = ui.cdFormatsList;
+  else if(toolButton == ui.removeDvdAspectRatio)
+    list = ui.dvdAspectRatiosList;
+
+  if(list)
+    delete list->takeItem(list->currentRow());
 }
 
 /*
@@ -152,71 +190,101 @@ void dbenumerations::slotSave(void)
   QString querystr("");
   QSqlQuery query(qmain->getDB());
 
-  /*
-  ** CD Formats
-  */
-
   qapp->setOverrideCursor(Qt::WaitCursor);
 
-  if(!qmain->getDB().transaction())
+  for(int i = 0; i < 2; i++)
     {
-      qapp->restoreOverrideCursor();
-      qmain->addError
-	(QString(tr("Database Error")),
-	 QString(tr("Unable to create a database transaction.")),
-	 qmain->getDB().lastError().text(), __FILE__, __LINE__);
-      QMessageBox::critical
-	(this, tr("BiblioteQ: Database Error"),
-	 tr("Unable to create a database transaction."));
-      return;
-    }
-
-  querystr = "DELETE FROM cd_formats";
-
-  if(!query.exec(querystr))
-    {
-      qapp->restoreOverrideCursor();
-      qmain->addError(QString(tr("Database Error")),
-		      QString(tr("An error occurred while attempting to "
-				 "remove the cd formats.")),
-		      query.lastError().text(), __FILE__, __LINE__);
-      goto db_rollback;
-    }
-
-  for(int i = 0; i < ui.cdFormatsList->count(); i++)
-    {
-      if(ui.cdFormatsList->item(i))
+      if(!qmain->getDB().transaction())
 	{
-	  querystr = QString("INSERT INTO cd_formats VALUES ('%1')").arg
-	    (ui.cdFormatsList->item(i)->text());
-
-	  if(!query.exec(querystr))
-	    {
-	      qapp->restoreOverrideCursor();
-	      qmain->addError
-		(QString(tr("Database Error")),
-		 QString(tr("Unable to create the cd format ")) +
-		 ui.cdFormatsList->item(i)->text() + QString(tr(" .")),
-		 query.lastError().text(), __FILE__, __LINE__);
-	      goto db_rollback;
-	    }
+	  qapp->restoreOverrideCursor();
+	  qmain->addError
+	    (QString(tr("Database Error")),
+	     QString(tr("Unable to create a database transaction.")),
+	     qmain->getDB().lastError().text(), __FILE__, __LINE__);
+	  QMessageBox::critical
+	    (this, tr("BiblioteQ: Database Error"),
+	     tr("Unable to create a database transaction."));
+	  return;
 	}
-    }
 
-  if(!qmain->getDB().commit())
-    {
-      qapp->restoreOverrideCursor();
-      qmain->addError
-	(QString(tr("Database Error")),
-	 QString(tr("Unable to commit the current database "
-		    "transaction.")),
-	 qmain->getDB().lastError().text(), __FILE__,
-	 __LINE__);
-      QMessageBox::critical(this,
-			    tr("BiblioteQ: Database Error"),
-			    tr("Unable to commit the current "
-			       "database transaction."));
-      return;
+      if(i == 0)
+	querystr = "DELETE FROM cd_formats";
+      else if(i == 1)
+	querystr = "DELETE FROM dvd_aspect_ratios";
+
+      if(!query.exec(querystr))
+	{
+	  qapp->restoreOverrideCursor();
+
+	  if(i == 0)
+	    qmain->addError(QString(tr("Database Error")),
+			    QString(tr("An error occurred while attempting to "
+				       "remove the cd formats.")),
+			    query.lastError().text(), __FILE__, __LINE__);
+	  else if(i == 1)
+	    qmain->addError(QString(tr("Database Error")),
+			    QString(tr("An error occurred while attempting to "
+				       "remove the dvd aspect_ratios.")),
+			    query.lastError().text(), __FILE__, __LINE__);
+
+	  goto db_rollback;
+	}
+
+      QString table("");
+      QString forerror("");
+      QListWidget *list = 0;
+
+      if(i == 0)
+	{
+	  table = "cd_formats";
+	  forerror = tr("cd format");
+	  list = ui.cdFormatsList;
+	}
+      else if(i == 1)
+	{
+	  table = "dvd_aspect_ratios";
+	  forerror = tr("dvd aspect ratio");
+	  list = ui.dvdAspectRatiosList;
+	}
+
+      if(list)
+	for(int j = 0; j < list->count(); j++)
+	  {
+	    if(list->item(j))
+	      {
+		querystr = QString("INSERT INTO %1 VALUES ('%2')").arg
+		  (table).arg
+		  (list->item(j)->text());
+
+		if(!query.exec(querystr))
+		  {
+		    qapp->restoreOverrideCursor();
+		    qmain->addError
+		      (QString(tr("Database Error")),
+		       QString(tr("Unable to create the ")) +
+		       forerror +
+		       list->item(j)->text() + QString(tr(" .")),
+		       query.lastError().text(), __FILE__, __LINE__);
+		    goto db_rollback;
+		  }
+	      }
+	  }
+
+      if(!qmain->getDB().commit())
+	{
+	  qapp->restoreOverrideCursor();
+	  qmain->addError
+	    (QString(tr("Database Error")),
+	     QString(tr("Unable to commit the current database "
+			"transaction.")),
+	     qmain->getDB().lastError().text(), __FILE__,
+	     __LINE__);
+	  QMessageBox::critical(this,
+				tr("BiblioteQ: Database Error"),
+				tr("Unable to commit the current "
+				   "database transaction."));
+	  return;
+	}
     }
 
   qapp->restoreOverrideCursor();
