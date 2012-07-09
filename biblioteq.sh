0347dfe9afd6e582b5b4969e37b4fabeafa96f62
textfield <textfield@876d2303-7a48-0410-9ec4-da127085e931>
2012-07-09 22:39:38 +0000

    Updated.

--- biblioteq.sh
+++ biblioteq.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-if [ -x /usr/local/biblioteq/BiblioteQ ]
+if [ -r /usr/local/biblioteq/BiblioteQ ] && [ -x /usr/local/biblioteq/BiblioteQ ]
 then
-    cd /usr/local/biblioteq
-    ./BiblioteQ &
+    cd /usr/local/biblioteq && ./BiblioteQ &
+    exit $?
 else
     exit 1
 fi
-
-exit 0
