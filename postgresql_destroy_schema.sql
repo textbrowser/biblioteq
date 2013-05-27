59fc65e80607556941e6914a05d76c23e197905a
textfield <textfield@876d2303-7a48-0410-9ec4-da127085e931>
2013-05-27 16:16:16 +0000

    Updated.

--- postgresql_destroy_schema.sql
+++ postgresql_destroy_schema.sql
@@ -1,7 +1,11 @@
 DROP DATABASE xbook_db;
 DROP ROLE biblioteq_administrator;
 DROP ROLE biblioteq_circulation;
+DROP ROLE biblioteq_circulation_librarian;
+DROP ROLE biblioteq_circulation_librarian_membership;
+DROP ROLE biblioteq_circulation_membership;
 DROP ROLE biblioteq_librarian;
+DROP ROLE biblioteq_librarian_membership;
 DROP ROLE biblioteq_membership;
 DROP ROLE biblioteq_patron;
 DROP USER xbook_admin;
