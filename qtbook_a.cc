c80a5c3c496ed302c6b848f6dbebb991ec17054d
textfield <textfield@876d2303-7a48-0410-9ec4-da127085e931>
2014-05-24 02:06:21 +0000

    Updated.

--- qtbook_a.cc
+++ qtbook_a.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2006 - Eternity Alexis Megas.
+** Copyright (c) 2006 - eternity, Alexis Megas.
 ** All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without
