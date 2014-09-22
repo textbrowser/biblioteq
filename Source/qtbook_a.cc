fc25688a742b2fe11c380d0b687f829be34e4fdd
textfield <textfield@876d2303-7a48-0410-9ec4-da127085e931>
2014-09-22 09:12:42 +0000

    Replaced eternity.

--- Source/qtbook_a.cc
+++ Source/qtbook_a.cc
@@ -1,5 +1,5 @@
 /*
-** Copyright (c) 2006 - eternity, Alexis Megas.
+** Copyright (c) 2006 - present, Alexis Megas.
 ** All rights reserved.
 **
 ** Redistribution and use in source and binary forms, with or without
