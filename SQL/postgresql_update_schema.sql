eddb346ea78575649affde918b7166052ceb9668
Alexis Megas <textbrowser@gmail.com>
2015-03-29 08:03:32 -0400

    Permissions.

--- SQL/postgresql_update_schema.sql
+++ SQL/postgresql_update_schema.sql
@@ -1056,6 +1056,7 @@ CREATE TABLE member_history_dnt
 	FOREIGN KEY(memberid) REFERENCES member(memberid) ON DELETE CASCADE
 );
 
+GRANT DELETE, INSERT, SELECT ON member_history_dnt TO biblioteq_administrator;
 GRANT SELECT ON member_history_dnt TO biblioteq_circulation;
+GRANT DELETE ON member_history_dnt TO biblioteq_membership;
 GRANT INSERT, SELECT, UPDATE ON member_history_dnt TO biblioteq_patron;
-REVOKE INSERT, SELECT, UPDATE ON member_history_dnt FROM biblioteq_administrator;
