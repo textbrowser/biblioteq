a9ef27e561341dd43f78e62241c7fcdb2fae3e07
Monster <textbrowser@gmail.com>
2017-03-24 20:00:31 -0400

    New.

---
+++ html/search/classes_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['userinfo_5fdiag_5fclass',['userinfo_diag_class',['../classuserinfo__diag__class.html',1,'']]]
+];
---
+++ html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "bcpu",
+  1: "bcpu"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
