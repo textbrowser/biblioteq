e8c70e7314ee442c5d493f2c62b44511184e3e57
Monster <textbrowser@gmail.com>
2018-01-09 08:28:52 -0500

    Debian files.

--- biblioteq.qt5.sh
+++ biblioteq.qt5.sh
@@ -2,6 +2,7 @@
 
 if [ -r /usr/local/biblioteq/BiblioteQ ] && [ -x /usr/local/biblioteq/BiblioteQ ]
 then
+    export LD_LIBRARY_PATH=/usr/local/biblioteq/Lib
     cd /usr/local/biblioteq && exec ./BiblioteQ -style fusion
     exit $?
 else
