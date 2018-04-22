53c0e59bf27d5f56dd70553dab91e34add0a8c6d
Monster <textbrowser@gmail.com>
2018-04-22 09:53:25 -0400

    Disable MIT shalalabingbang.

--- biblioteq.sh
+++ biblioteq.sh
@@ -2,6 +2,8 @@
 
 if [ -r /usr/local/biblioteq/BiblioteQ ] && [ -x /usr/local/biblioteq/BiblioteQ ]
 then
+    # Disable https://en.wikipedia.org/wiki/MIT-SHM.
+    export QT_X11_NO_MITSHM=1
     cd /usr/local/biblioteq && exec ./BiblioteQ
     exit $?
 else
