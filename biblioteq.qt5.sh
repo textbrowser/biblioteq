b9e3fb499507c71ee1b57371de5f615e0c6fa527
Monster <textbrowser@gmail.com>
2018-05-04 08:40:34 -0400

    New Debian packaging.

--- biblioteq.qt5.sh
+++ biblioteq.qt5.sh
@@ -2,7 +2,6 @@
 
 if [ -r /usr/local/biblioteq/BiblioteQ ] && [ -x /usr/local/biblioteq/BiblioteQ ]
 then
-    export LD_LIBRARY_PATH=/usr/local/biblioteq/Lib
     # Disable https://en.wikipedia.org/wiki/MIT-SHM.
     export QT_X11_NO_MITSHM=1
     cd /usr/local/biblioteq && exec ./BiblioteQ -style fusion
