2202dc9aadfcf600b63a6f2facde7bfd191ef744
textbrowser@gmail.com <textbrowser@gmail.com>
2019-06-06 07:30:53 -0400

    Qt4.

--- biblioteq.sh
+++ biblioteq.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ -r /usr/local/biblioteq/BiblioteQ ] && [ -x /usr/local/biblioteq/BiblioteQ ]
+if [ -r /usr/local/biblioteq-qt4/BiblioteQ ] && [ -x /usr/local/biblioteq-qt4/BiblioteQ ]
 then
     # Disable https://en.wikipedia.org/wiki/MIT-SHM.
     export QT_X11_NO_MITSHM=1
-    cd /usr/local/biblioteq && exec ./BiblioteQ
+    cd /usr/local/biblioteq-qt4 && exec ./BiblioteQ
     exit $?
 else
     exit 1
