b52ddd37dde5c4ee719ead225a1af1d5fadd2302
textbrowser@gmail.com <textbrowser@gmail.com>
2019-08-03 13:11:23 -0400

    Format changes.

--- Source/biblioteq_myqstring.cc
+++ Source/biblioteq_myqstring.cc
@@ -9,20 +9,6 @@ biblioteq_myqstring::biblioteq_myqstring(const char *str):QString(str)
 {
 }
 
-QString biblioteq_myqstring::prepConfigString
-(const QString &str,
- const bool ignore_embedded_comments)
-{
-  QString mystr = str;
-
-  if(mystr.trimmed().startsWith("#"))
-    return mystr.trimmed();
-  else if(mystr.contains("#") && !ignore_embedded_comments)
-    mystr.resize(mystr.indexOf('#'));
-
-  return mystr.trimmed();
-}
-
 QString biblioteq_myqstring::escape(const QString &str,
 				    const bool caseinsensitive)
 {
@@ -32,6 +18,19 @@ QString biblioteq_myqstring::escape(const QString &str,
 
   if(caseinsensitive)
     mystr = mystr.toLower();
+
+  return mystr.trimmed();
+}
+
+QString biblioteq_myqstring::prepConfigString
+(const QString &str, const bool ignore_embedded_comments)
+{
+  QString mystr = str;
+
+  if(mystr.trimmed().startsWith("#"))
+    return mystr.trimmed();
+  else if(mystr.contains("#") && !ignore_embedded_comments)
+    mystr.resize(mystr.indexOf('#'));
 
   return mystr.trimmed();
 }
