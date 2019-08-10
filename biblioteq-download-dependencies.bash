6254dc12da4b00b3a5c8c122f206bb543e1e9cd8
textbrowser@gmail.com <textbrowser@gmail.com>
2019-08-10 08:16:02 -0400

    PQ 9.6.15.1.

--- biblioteq-download-dependencies.bash
+++ biblioteq-download-dependencies.bash
@@ -9,12 +9,13 @@ postgresql=postgresql.zip
 rm -f $postgresql
 wget --output-document=$postgresql \
      --progress=bar \
-     "https://get.enterprisedb.com/postgresql/postgresql-9.6.14-3-windows-binaries.zip"
+     "https://get.enterprisedb.com/postgresql/postgresql-9.6.15-1-windows-binaries.zip"
 unzip $postgresql
-mv pgsql/bin/libeay32.dll Libraries.win32/postgresql/.
+mv pgsql/bin/libcrypto-1_1.dll Libraries.win32/postgresql/.
 mv pgsql/bin/libiconv-2.dll Libraries.win32/postgresql/.
 mv pgsql/bin/libintl-8.dll Libraries.win32/postgresql/.
 mv pgsql/bin/libpq.dll Libraries.win32/postgresql/.
+mv pgsql/bin/libssl-1_1.dll Libraries.win32/postgresql/.
 mv pgsql/bin/libxml2.dll Libraries.win32/postgresql/.
 mv pgsql/bin/libxslt.dll Libraries.win32/postgresql/.
 chmod +r,+w,-x Libraries.win32/postgresql/*
