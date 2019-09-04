77fc48c2ffc461988aeb6abe48b5391bf10f1f73
textbrowser@gmail.com <textbrowser@gmail.com>
2019-09-03 20:51:20 -0400

    Added Call Number to All Requested.

--- Source/biblioteq_main_table.cc
+++ Source/biblioteq_main_table.cc
@@ -192,7 +192,9 @@ void biblioteq_main_table::setColumns(const QString &username,
       list.append(tr("Title"));
       list.append(tr("ID Number"));
 
-      if(type == "All Overdue" || type == "All Reserved")
+      if(type == "All Overdue" ||
+	 type == "All Requested" ||
+	 type == "All Reserved")
 	list.append(tr("Call Number"));
 
       list.append(tr("Publisher"));
@@ -206,7 +208,9 @@ void biblioteq_main_table::setColumns(const QString &username,
       m_columnHeaderIndexes.append("Title");
       m_columnHeaderIndexes.append("ID Number");
 
-      if(type == "All Overdue" || type == "All Reserved")
+      if(type == "All Overdue" ||
+	 type == "All Requested" ||
+	 type == "All Reserved")
 	m_columnHeaderIndexes.append("Call Number");
 
       m_columnHeaderIndexes.append("Publisher");
