cff6fb8d6b546d9546f69d553b49e09e01a33f66
textbrowser@gmail.com <textbrowser@gmail.com>
2019-11-16 13:42:19 -0500

    Default style.

--- biblioteq.qt5.sh
+++ biblioteq.qt5.sh
@@ -4,7 +4,7 @@ if [ -r /usr/local/biblioteq/BiblioteQ ] && [ -x /usr/local/biblioteq/BiblioteQ 
 then
     # Disable https://en.wikipedia.org/wiki/MIT-SHM.
     export QT_X11_NO_MITSHM=1
-    cd /usr/local/biblioteq && exec ./BiblioteQ -style Fusion
+    cd /usr/local/biblioteq && exec ./BiblioteQ
     exit $?
 else
     exit 1
