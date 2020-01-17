1637b93bc98331650ee28d8897835ef88bfb8ca2
textbrowser@gmail.com <textbrowser@gmail.com>
2020-01-17 09:34:43 -0500

    Reset checkbox.

--- Source/biblioteq_c.cc
+++ Source/biblioteq_c.cc
@@ -3372,6 +3372,7 @@ void biblioteq::slotModifyBorrower(void)
 	textfield->setCursorPosition(0);
     }
 
+  userinfo_diag->m_userinfo.allow_any_email->setChecked(false);
   userinfo_diag->m_userinfo.memberid->setReadOnly(true);
   userinfo_diag->m_userinfo.prevTool->setVisible(true);
   userinfo_diag->m_userinfo.nextTool->setVisible(true);
