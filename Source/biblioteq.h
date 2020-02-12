a7aecaa419fbb8cb9bca189c5382e55a2483d24d
textbrowser@gmail.com <textbrowser@gmail.com>
2020-02-12 17:26:07 -0500

    Future release.

--- Source/biblioteq.h
+++ Source/biblioteq.h
@@ -1,7 +1,7 @@
 #ifndef _BIBLIOTEQ_H_
 #define _BIBLIOTEQ_H_
 
-#define BIBLIOTEQ_VERSION "2020.01.01"
+#define BIBLIOTEQ_VERSION "2020.03.01"
 
 #include "biblioteq_book.h"
 #include "biblioteq_callnum_table_item.h"
