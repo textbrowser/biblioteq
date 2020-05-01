2812e0e7d6480f7ee971d9d48914e3233578c9a9
textbrowser@gmail.com <textbrowser@gmail.com>
2020-05-01 07:06:12 -0400

    New file.

---
+++ Source/biblioteq_open_library.h
@@ -0,0 +1,11 @@
+#ifndef _BIBLIOTEQ_OPEN_LIBRARY_H_
+#define _BIBLIOTEQ_OPEN_LIBRARY_H_
+
+class biblioteq_open_library
+{
+ public:
+  biblioteq_open_library(void);
+  ~biblioteq_open_library();
+};
+
+#endif
