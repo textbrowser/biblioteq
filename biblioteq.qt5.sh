d2ba7d29b5af593f952e3ee31b098220382735c7
textbrowser@gmail.com <textbrowser@gmail.com>
2020-05-26 21:10:41 -0400

    High DPI.

--- biblioteq.qt5.sh
+++ biblioteq.qt5.sh
@@ -2,6 +2,8 @@
 
 if [ -r /usr/local/biblioteq/BiblioteQ ] && [ -x /usr/local/biblioteq/BiblioteQ ]
 then
+    export QT_AUTO_SCREEN_SCALE_FACTOR=1
+
     # Disable https://en.wikipedia.org/wiki/MIT-SHM.
 
     export QT_X11_NO_MITSHM=1
