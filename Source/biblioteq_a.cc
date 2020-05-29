0b5b7051612d650d351cee9c588f7bda1db3baa6
textbrowser@gmail.com <textbrowser@gmail.com>
2020-05-29 09:01:12 -0400

    New About.

--- Source/biblioteq_a.cc
+++ Source/biblioteq_a.cc
@@ -1922,7 +1922,7 @@ void biblioteq::slotAbout(void)
     (tr("<html>BiblioteQ Version %1<br>"
 	"Architecture %4.<br>"
 	"Compiled on %2, %3.<br>"
-	"Copyright (c) 2005 - present, Petya Stormware.<br>"
+	"Copyright (c) 2005 - present, Fire Flight.<br>"
 	"Faenza icons.<br>"
 #ifdef BIBLIOTEQ_POPPLER_VERSION_DEFINED
 	"Poppler version %5.<br>"
