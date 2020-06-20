28f427efada7c6ce6137762e59304857a6cbb024
textbrowser@gmail.com <textbrowser@gmail.com>
2020-06-20 17:24:16 -0400

    SQLite 3.32.3.

--- biblioteq-download-dependencies.bash
+++ biblioteq-download-dependencies.bash
@@ -29,7 +29,7 @@ fi
 
 # SQLite Binaries
 
-sqlite=sqlite-dll-win32-x86-3320200.zip
+sqlite=sqlite-dll-win32-x86-3320300.zip
 
 rm -f $sqlite
 wget --progress=bar https://sqlite.org/2020/$sqlite
@@ -45,7 +45,7 @@ fi
 
 # SQLite Source
 
-sqlite=sqlite-amalgamation-3320200.zip
+sqlite=sqlite-amalgamation-3320300.zip
 
 rm -f $sqlite
 wget --progress=bar https://sqlite.org/2020/$sqlite
@@ -57,7 +57,7 @@ else
     echo "Cannot read $sqlite."
 fi
 
-sqlite=sqlite-amalgamation-3320200
+sqlite=sqlite-amalgamation-3320300
 
 if [ -r $sqlite ]; then
     mv $sqlite/*.h Include.win32/sqlite3/.
