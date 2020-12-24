26c8d36e464308f531d4eb542240e2d6a331fdd6
textbrowser@gmail.com <textbrowser@gmail.com>
2020-12-23 22:10:40 -0500

    New enumerator.

--- Source/biblioteq_files.h
+++ Source/biblioteq_files.h
@@ -15,6 +15,17 @@ class biblioteq_files: public QMainWindow
   void reset(void);
 
  private:
+  enum Columns
+    {
+     COMPRESSED_SIZE = 3,
+     DESCRIPTION = 1,
+     DIGEST = 2,
+     FILE = 0,
+     ITEM_TYPE = 5,
+     MYOID = 6,
+     PARENT_TITLE = 4
+    };
+
   Ui_files m_ui;
   biblioteq *m_biblioteq;
   void changeEvent(QEvent *event);
