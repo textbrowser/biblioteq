e1b198c81197f2d18b5848d8737cd691914736c1
textbrowser@gmail.com <textbrowser@gmail.com>
2020-12-31 23:23:28 -0500

    New information.

--- freebsd.sh
+++ freebsd.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# Do not remove the xbook_db.sql file after compressing it.
+
 pg_dump -U postgres --clean --file=/disk0/biblioteq.d/xbook_db.sql xbook_db
 pg_dumpall -U postgres --clean --globals-only --file=/disk0/biblioteq.d/globals.sql
 gzip --force /disk0/biblioteq.d/xbook_db.sql
