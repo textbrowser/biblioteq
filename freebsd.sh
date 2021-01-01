ca4f1bcf458e6009ca92f8bb26acf9c811b7a83e
textbrowser@gmail.com <textbrowser@gmail.com>
2020-12-31 23:12:07 -0500

    Force overwrite.

--- freebsd.sh
+++ freebsd.sh
@@ -2,4 +2,4 @@
 
 pg_dump -U postgres --clean --file=/disk0/biblioteq.d/xbook_db.sql xbook_db
 pg_dumpall -U postgres --clean --globals-only --file=/disk0/biblioteq.d/globals.sql
-gzip /disk0/biblioteq.d/xbook_db.sql
+gzip --force /disk0/biblioteq.d/xbook_db.sql
