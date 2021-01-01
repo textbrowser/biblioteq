595baf32ac41b93a71e416d6bfc60cd8f0a953fb
textbrowser@gmail.com <textbrowser@gmail.com>
2021-01-01 15:59:00 -0500

    Unique file names.

---
+++ SQL/freebsd-copy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+date=$(date "+%Y%m%d")
+
+pg_dump -U postgres --clean --file=/disk0/biblioteq.d/xbook_db.$date.sql xbook_db
+pg_dumpall -U postgres --clean --globals-only --file=/disk0/biblioteq.d/globals.$date.sql
+gzip --force --keep /disk0/biblioteq.d/xbook_db.$date.sql
--- SQL/freebsd.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Do not remove the xbook_db.sql file after compressing it.
-
-pg_dump -U postgres --clean --file=/disk0/biblioteq.d/xbook_db.sql xbook_db
-pg_dumpall -U postgres --clean --globals-only --file=/disk0/biblioteq.d/globals.sql
-gzip --force /disk0/biblioteq.d/xbook_db.sql
