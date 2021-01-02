812acf8938a8288621a83fae74fb5b53deb2488d
textbrowser@gmail.com <textbrowser@gmail.com>
2021-01-01 20:36:50 -0500

    Format changes.

--- SQL/biblioteq-copy.sh
+++ SQL/biblioteq-copy.sh
@@ -2,6 +2,8 @@
 
 date=$(date "+%Y%m%d")
 
-pg_dump -U postgres --clean --file=/disk0/biblioteq.d/xbook_db.$date.sql xbook_db
-pg_dumpall -U postgres --clean --globals-only --file=/disk0/biblioteq.d/globals.$date.sql
+pg_dump -U postgres \
+	--clean --file=/disk0/biblioteq.d/xbook_db.$date.sql xbook_db
+pg_dumpall -U postgres \
+	   --clean --globals-only --file=/disk0/biblioteq.d/globals.$date.sql
 gzip --force --keep /disk0/biblioteq.d/xbook_db.$date.sql
