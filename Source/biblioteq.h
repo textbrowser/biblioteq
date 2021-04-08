d3b7e1120694c0625a5e55f9f70aefc06b976adc
textbrowser@gmail.com <textbrowser@gmail.com>
2021-04-07 21:49:22 -0400

    New release.

--- Source/biblioteq.h
+++ Source/biblioteq.h
@@ -1,7 +1,7 @@
 #ifndef _BIBLIOTEQ_H_
 #define _BIBLIOTEQ_H_
 
-#define BIBLIOTEQ_VERSION "2021.02.12"
+#define BIBLIOTEQ_VERSION "2021.04.10"
 
 #include "biblioteq_book.h"
 #include "biblioteq_callnum_table_item.h"
