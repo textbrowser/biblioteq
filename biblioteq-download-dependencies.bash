3a77c56f12b429224778e01e09263b06954e9df8
textbrowser@gmail.com <textbrowser@gmail.com>
2021-04-10 12:44:50 -0400

    Future PQ 10.16.

--- biblioteq-download-dependencies.bash
+++ biblioteq-download-dependencies.bash
@@ -9,17 +9,16 @@ postgresql=postgresql.zip
 rm -f $postgresql
 wget --output-document=$postgresql \
      --progress=bar \
-     "https://get.enterprisedb.com/postgresql/postgresql-9.6.21-1-windows-binaries.zip"
+     "https://get.enterprisedb.com/postgresql/postgresql-10.16-2-windows-binaries.zip"
 
 if [ -r $postgresql ]; then
     unzip -q $postgresql
-    mv pgsql/bin/libcrypto-1_1.dll Libraries.win32/postgresql/.
-    mv pgsql/bin/libiconv-2.dll Libraries.win32/postgresql/.
-    mv pgsql/bin/libintl-8.dll Libraries.win32/postgresql/.
-    mv pgsql/bin/libpq.dll Libraries.win32/postgresql/.
-    mv pgsql/bin/libssl-1_1.dll Libraries.win32/postgresql/.
-    mv pgsql/bin/libxml2.dll Libraries.win32/postgresql/.
-    mv pgsql/bin/libxslt.dll Libraries.win32/postgresql/.
+    mv pgsql/lib/libpq.dll Libraries.win32/postgresql/.
+    mv 'pgsql/pgAdmin 4/bin/libiconv-2.dll' Libraries.win32/postgresql/.
+    mv 'pgsql/pgAdmin 4/bin/libintl-8.dll' Libraries.win32/postgresql/.
+    mv 'pgsql/pgAdmin 4/venv/DLLs/libcrypto-1_1.dll' \
+       Libraries.win32/postgresql/.
+    mv 'pgsql/pgAdmin 4/venv/DLLs/libssl-1_1.dll' Libraries.win32/postgresql/.
     chmod +r,+w,-x Libraries.win32/postgresql/*
     rm -f $postgresql
     rm -fr pgsql
