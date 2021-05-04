23e1ad61d731f096c2bccd2586d3e3972a021f0e
textbrowser@gmail.com <textbrowser@gmail.com>
2021-05-04 06:19:26 -0400

    New future.

--- Source/biblioteq.h
+++ Source/biblioteq.h
@@ -1,7 +1,7 @@
 #ifndef _BIBLIOTEQ_H_
 #define _BIBLIOTEQ_H_
 
-#define BIBLIOTEQ_VERSION "2021.04.10"
+#define BIBLIOTEQ_VERSION "2021.05.15"
 
 #include "biblioteq_book.h"
 #include "biblioteq_callnum_table_item.h"
