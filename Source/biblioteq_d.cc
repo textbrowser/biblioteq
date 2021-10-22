2fe82d226f1913db01252abadd070e2c3806b4b3
textbrowser@gmail.com <textbrowser@gmail.com>
2021-10-22 15:17:24 -0400

    Android.

--- Source/biblioteq_d.cc
+++ Source/biblioteq_d.cc
@@ -6,8 +6,12 @@ void biblioteq::slotShowFiles(void)
   if(!m_files)
     m_files = new biblioteq_files(this);
 
+#ifdef Q_OS_ANDROID
+  m_files->showMaximized();
+#else
   biblioteq_misc_functions::center(m_files, this);
   m_files->showNormal();
+#endif
   m_files->activateWindow();
   m_files->raise();
 }
