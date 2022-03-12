dd5f2141996526658837d6195ab1d044c45f5096
textbrowser@gmail.com <textbrowser@gmail.com>
2022-03-12 09:39:01 -0500

    New PQ and SQ libraries.

--- biblioteq-download-dependencies.bash
+++ biblioteq-download-dependencies.bash
@@ -9,7 +9,7 @@ postgresql=postgresql.zip
 rm -f $postgresql
 wget --output-document=$postgresql \
      --progress=bar \
-     "https://get.enterprisedb.com/postgresql/postgresql-10.19-1-windows-binaries.zip"
+     "https://get.enterprisedb.com/postgresql/postgresql-10.20-1-windows-binaries.zip"
 
 if [ -r $postgresql ]; then
     unzip -q $postgresql
@@ -29,7 +29,7 @@ fi
 
 # SQLite Binaries
 
-sqlite=sqlite-dll-win32-x86-3370200.zip
+sqlite=sqlite-dll-win32-x86-3380100.zip
 
 rm -f $sqlite
 wget --progress=bar https://sqlite.org/2022/$sqlite
@@ -45,7 +45,7 @@ fi
 
 # SQLite Source
 
-sqlite=sqlite-amalgamation-3370200.zip
+sqlite=sqlite-amalgamation-3380100.zip
 
 rm -f $sqlite
 wget --progress=bar https://sqlite.org/2022/$sqlite
@@ -57,7 +57,7 @@ else
     echo "Cannot read $sqlite."
 fi
 
-sqlite=sqlite-amalgamation-3370200
+sqlite=sqlite-amalgamation-3380100
 
 if [ -r $sqlite ]; then
     cp $sqlite/*.h Android/sqlite3/.
