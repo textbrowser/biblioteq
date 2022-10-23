73161eaef18272fe92d3b6f7f025da21ce8149e1
textbrowser <textbrowser@gmail.com>
2022-10-23 10:53:12 -0400

    Focus on widget.

--- Source/biblioteq_batch_activities.cc
+++ Source/biblioteq_batch_activities.cc
@@ -388,6 +388,7 @@ void biblioteq_batch_activities::slotReset(void)
   m_ui.borrow_table->clearContents();
   m_ui.borrow_table->setRowCount(0);
   m_ui.member_id->clear();
+  m_ui.member_id->setFocus();
   m_ui.scan->clear();
   m_ui.scan_type->setCurrentIndex(0);
 }
