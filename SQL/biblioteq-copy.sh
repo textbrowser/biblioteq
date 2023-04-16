aae6fb19928cd3dd369024fa1d7b9ac73b1248ea
textbrowser <textbrowser@gmail.com>
2023-04-15 22:08:29 -0400

    Remove permissions.

--- SQL/biblioteq-copy.sh
+++ SQL/biblioteq-copy.sh
@@ -6,3 +6,4 @@ date=$(date "+%Y%m%d")
 pg_dump -U postgres --clean --file=xbook_db.$date.sql xbook_db
 pg_dumpall -U postgres --clean --globals-only --file=globals.$date.sql
 gzip --force --keep xbook_db.$date.sql
+chmod -rw globals.$date.sql xbook_db.$date.sql
