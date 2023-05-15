46e43ada44e75e310e2aad9b285102b97d7640d6
textbrowser <textbrowser@gmail.com>
2023-05-15 10:31:54 -0400

    Perhaps release date.

--- Distributions/build_debian_amd64.bash
+++ Distributions/build_debian_amd64.bash
@@ -39,7 +39,7 @@ mkdir -p biblioteq-debian/usr/share/applications
 cp -p ./biblioteq.desktop biblioteq-debian/usr/share/applications/.
 cp -pr ./DEBIAN biblioteq-debian/.
 cp -r ./usr/local/biblioteq biblioteq-debian/usr/local/.
-fakeroot dpkg-deb --build biblioteq-debian BiblioteQ-2023.07.07_amd64.deb
+fakeroot dpkg-deb --build biblioteq-debian BiblioteQ-2023.06.06_amd64.deb
 make distclean
 rm -fr ./usr
 rm -fr biblioteq-debian
