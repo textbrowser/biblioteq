74415ee702e5b67d93a17006c90d26a7ee05edee
textbrowser <textbrowser@gmail.com>
2023-05-20 12:39:54 -0400

    Format.

--- Tools/bump-version.sh
+++ Tools/bump-version.sh
@@ -1,24 +1,27 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # SPDX-License-Identifier: MIT
 
 VERSION=$1
 
 if [ -z "$VERSION" ]; then
-  echo "Usage: $0 <VERSION>"
-  exit 1
+    echo "Usage: $0 <VERSION>"
+    exit 1
 fi
 
-# debian release packages
+# Debian release packages.
+
 for FOLDER in DEBIAN DEBIAN-POWERPC RASPBIAN PIOS64; do
-  sed -i "s/Version: .*/Version: $VERSION/" $FOLDER/control
+    sed -i "s/Version: .*/Version: $VERSION/" $FOLDER/control
 done
 
-# version config
+# Version configuration.
+
 FILE="Source/biblioteq.h"
 sed -i 's/\(BIBLIOTEQ_VERSION "\)[0-9]\+\(\.[0-9]\+\)*"/\1'"$VERSION"'"/' $FILE
 
-# android manifest
+# Android manifest file.
+
 FILE="android/AndroidManifest.xml"
 sed -i 's/\(android:versionName="\)[0-9]\+\(\.[0-9]\+\)*"/\1'"$VERSION"'"/' $FILE
 sed -i 's/\(android:versionCode="\)[0-9]\+\(\.[0-9]\+\)*"/\1'"${VERSION//./}"'"/' $FILE
