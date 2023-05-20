b3ddeb42f8546ae5c55973904d8848936a621cde
textbrowser <textbrowser@gmail.com>
2023-05-20 12:32:21 -0400

    YAZ 5.34.

--- Include.win32/yaz/yaz-version.h
+++ Include.win32/yaz/yaz-version.h
@@ -33,16 +33,16 @@
 #include <yaz/yconfig.h>
 
 /** \brief YAZ version as string */
-#define YAZ_VERSION "5.31.1"
+#define YAZ_VERSION "5.34.0"
 
 /** \brief YAZ version as integer (for comparison purposes) */
-#define YAZ_VERSIONL 0x51f01
+#define YAZ_VERSIONL 0x52200
 
 /** \brief YAZ file version for YAZ DLL (resource) */
-#define YAZ_FILEVERSION 5,31,1,1
+#define YAZ_FILEVERSION 5,34,0,1
 
 /** \brief SHA1 ID for YAZ (Git) */
-#define YAZ_VERSION_SHA1 "c3cea881e3e7e80b069ddd1429994e58841acb14"
+#define YAZ_VERSION_SHA1 ""
 
 YAZ_BEGIN_CDECL
 
