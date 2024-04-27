1fc98c34bfece006ff8ab199bd571fb16cabf528
textbrowser <textbrowser@gmail.com>
2024-04-27 13:11:42 -0400

    Doxygen.

--- Documentation/Doxygen/html/search/all_1.js
+++ Documentation/Doxygen/html/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['cocoainitializer_48',['CocoaInitializer',['../classCocoaInitializer.html',1,'']]]
+  ['cocoainitializer_0',['CocoaInitializer',['../classCocoaInitializer.html',1,'']]]
 ];
--- Documentation/Doxygen/html/search/classes_3.js
+++ Documentation/Doxygen/html/search/classes_3.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['swifty_103',['swifty',['../classswifty.html',1,'']]]
+  ['swifty_0',['swifty',['../classswifty.html',1,'']]]
 ];
