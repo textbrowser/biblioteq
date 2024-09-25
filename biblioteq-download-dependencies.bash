8399459c7e05d81122566ac14dd3fd0ca8ce91df
textbrowser <textbrowser@gmail.com>
2024-09-25 09:46:58 -0400

    File and host.

--- biblioteq-download-dependencies.bash
+++ biblioteq-download-dependencies.bash
@@ -14,14 +14,16 @@ fi
 postgresql=postgresql.zip
 
 rm -f $postgresql
-wget --output-document=$postgresql \
-     --progress=bar \
-     "https://get.enterprisedb.com/postgresql/postgresql-15.8-1-windows-x64-binaries.zip"
 
-if [ -r $postgresql ]; then
+filename="/postgresql/postgresql-15.8-1-windows-x64-binaries.zip"
+host="https://get.enterprisedb.com"
+
+wget --output-document=$postgresql --progress=bar $host$filename
+
+if [ -r $postgresql ]
+then
     unzip -q $postgresql
     mkdir -p Distributions/Windows/Libraries.64
-    mv pgsql/bin/libpq.dll Distributions/Windows/Libraries.64/.
     mv 'pgsql/pgAdmin 4/runtime/libcrypto-3-x64.dll' \
        Distributions/Windows/Libraries.64/.
     mv 'pgsql/pgAdmin 4/runtime/libiconv-2.dll' \
@@ -30,6 +32,7 @@ if [ -r $postgresql ]; then
        Distributions/Windows/Libraries.64/.
     mv 'pgsql/pgAdmin 4/runtime/libssl-3-x64.dll' \
        Distributions/Windows/Libraries.64/.
+    mv pgsql/bin/libpq.dll Distributions/Windows/Libraries.64/.
     mv pgsql/bin/libx*.dll Distributions/Windows/Libraries.64/.
     chmod +r,+w,-x Distributions/Windows/Libraries.64/*.dll
     rm -f $postgresql
