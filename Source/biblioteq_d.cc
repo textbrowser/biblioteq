d7c9290ff5b8c3bacbedc1a65c2b3e5b49e05733
textbrowser <textbrowser@gmail.com>
2025-07-14 11:51:33 -0400

    Automatic type.

--- Source/biblioteq_d.cc
+++ Source/biblioteq_d.cc
@@ -821,11 +821,10 @@ void biblioteq::prepareTabWidgetCloseButtons(void)
   QApplication::setOverrideCursor(Qt::WaitCursor);
   ui.tab->setTabsClosable(ui.tab->count() > 1);
 
-  static QList<QTabBar::ButtonPosition> const list
-    (QList<QTabBar::ButtonPosition> ()
-     << QTabBar::LeftSide
-     << QTabBar::RightSide);
-  static const int index = 0;
+  auto const list
+    (QList<QTabBar::ButtonPosition> () << QTabBar::LeftSide
+                                       << QTabBar::RightSide);
+  const int static index = 0;
 
   for(int i = 0; i < list.size(); i++)
     {
