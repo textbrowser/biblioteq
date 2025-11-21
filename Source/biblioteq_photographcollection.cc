4285b1bb9ea82ed9d665172399c238748c61a21f
textbrowser <textbrowser@gmail.com>
2025-11-20 20:38:40 -0500

    MacOS.

--- Source/biblioteq_photographcollection.cc
+++ Source/biblioteq_photographcollection.cc
@@ -242,18 +242,20 @@ biblioteq_photographcollection::biblioteq_photographcollection
 
 #ifdef Q_OS_MACOS
   if(!biblioteq_misc_functions::isEnvironmentSet("QT_STYLE_OVERRIDE"))
-    foreach(auto tool_button, findChildren<QToolButton *> ())
-      {
+    {
+      foreach(auto tool_button, findChildren<QToolButton *> ())
+	{
 #if (QT_VERSION < QT_VERSION_CHECK(5, 10, 0))
-	tool_button->setStyleSheet
-	  ("QToolButton {border: none; padding-right: 10px}"
-	   "QToolButton::menu-button {border: none;}");
+	  tool_button->setStyleSheet
+	    ("QToolButton {border: none; padding-right: 10px}"
+	     "QToolButton::menu-button {border: none;}");
 #else
-	tool_button->setStyleSheet
-	  ("QToolButton {border: none; padding-right: 15px}"
-	   "QToolButton::menu-button {border: none; width: 15px;}");
+	  tool_button->setStyleSheet
+	    ("QToolButton {border: none; padding-right: 15px}"
+	     "QToolButton::menu-button {border: none; width: 15px;}");
 #endif
-      }
+	}
+    }
 #endif
 
   pc.splitter->setStretchFactor(0, 0);
