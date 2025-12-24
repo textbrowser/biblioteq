e2725b6d90e1df5c24baf4dad427aaf02ee6c215
textbrowser <textbrowser@gmail.com>
2025-12-24 10:02:04 -0500

    What are the results?

--- Tools/biblioteq-prepare-apk-name.bash
+++ Tools/biblioteq-prepare-apk-name.bash
@@ -14,11 +14,29 @@ for i in $(find . -iname '*.apk')
 do
     if [[ "$i" == *"arm64"* ]]
     then
-	mv $i ~/BiblioteQ-"$VERSION"_arm64.apk
+	filename="~/BiblioteQ-"$VERSION"_arm64.apk"
+
+	mv $i "$filename"
+
+	if [ $? -eq 0 ]
+	then
+	    echo "Created the file $filename."
+	else
+	    echo "Could not create $filename."
+	fi
     fi
 
     if [[ "$i" == *"armeabi"* ]]
     then
-	mv $i ~/BiblioteQ-"$VERSION"_arm32.apk
+	filename="~/BiblioteQ-"$VERSION"_arm32.apk"
+
+	mv $i "$filename"
+
+	if [ $? -eq 0 ]
+	then
+	    echo "Created the file $filename."
+	else
+	    echo "Could not create $filename."
+	fi
     fi
 done
