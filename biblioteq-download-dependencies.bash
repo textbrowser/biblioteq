dca976ad7dba6887a32f1fc4d284829ff5258c8b
textbrowser <textbrowser@gmail.com>
2025-12-24 08:42:06 -0500

    Windows.

--- biblioteq-download-dependencies.bash
+++ biblioteq-download-dependencies.bash
@@ -27,14 +27,11 @@ if [ -r $postgresql ]
 then
     unzip -q $postgresql
     mkdir -p Distributions/Windows/Libraries.64
-    mv 'pgsql/pgAdmin 4/runtime/libcrypto-3-x64.dll' \
-       Distributions/Windows/Libraries.64/.
-    mv 'pgsql/pgAdmin 4/runtime/libiconv-2.dll' \
-       Distributions/Windows/Libraries.64/.
-    mv 'pgsql/pgAdmin 4/runtime/libintl-9.dll' \
-       Distributions/Windows/Libraries.64/.
-    mv 'pgsql/pgAdmin 4/runtime/libssl-3-x64.dll' \
-       Distributions/Windows/Libraries.64/.
+    mv 'pgsql/bin/libcrypto-3-x64.dll' Distributions/Windows/Libraries.64/.
+    mv 'pgsql/bin/libiconv-2.dll' Distributions/Windows/Libraries.64/.
+    mv 'pgsql/bin/libintl-9.dll' Distributions/Windows/Libraries.64/.
+    mv 'pgsql/bin/libssl-3-x64.dll' Distributions/Windows/Libraries.64/.
+    mv 'pgsql/bin/zlib1.dll' Distributions/Windows/Libraries.64/.
     mv pgsql/bin/libpq.dll Distributions/Windows/Libraries.64/.
     mv pgsql/bin/libx*.dll Distributions/Windows/Libraries.64/.
     chmod +r,+w,-x Distributions/Windows/Libraries.64/*.dll
