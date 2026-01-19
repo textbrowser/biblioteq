52f7c02ce895875b1b829f0d911b7a5159830619
textbrowser <textbrowser@gmail.com>
2026-01-19 18:08:24 -0500

    Quotes.

--- biblioteq-download-dependencies.bash
+++ biblioteq-download-dependencies.bash
@@ -14,28 +14,28 @@ rc=0
 
 # PostgreSQL
 
-postgresql=postgresql.zip
+postgresql="postgresql.zip"
 
-rm -f $postgresql
+rm -f "$postgresql"
 
 filename="/postgresql/postgresql-16.11-2-windows-x64-binaries.zip"
 host="https://get.enterprisedb.com"
 
-wget --output-document=$postgresql --progress=bar $host$filename
+wget --output-document="$postgresql" --progress=bar "$host$filename"
 
-if [ -r $postgresql ]
+if [ -r "$postgresql" ]
 then
-    unzip -q $postgresql
+    unzip -q "$postgresql"
     mkdir -p Distributions/Windows/Libraries.64
-    mv 'pgsql/bin/libcrypto-3-x64.dll' Distributions/Windows/Libraries.64/.
-    mv 'pgsql/bin/libiconv-2.dll' Distributions/Windows/Libraries.64/.
-    mv 'pgsql/bin/libintl-9.dll' Distributions/Windows/Libraries.64/.
-    mv 'pgsql/bin/libssl-3-x64.dll' Distributions/Windows/Libraries.64/.
-    mv 'pgsql/bin/zlib1.dll' Distributions/Windows/Libraries.64/.
+    mv pgsql/bin/libcrypto-3-x64.dll Distributions/Windows/Libraries.64/.
+    mv pgsql/bin/libiconv-2.dll Distributions/Windows/Libraries.64/.
+    mv pgsql/bin/libintl-9.dll Distributions/Windows/Libraries.64/.
+    mv pgsql/bin/libssl-3-x64.dll Distributions/Windows/Libraries.64/.
+    mv pgsql/bin/zlib1.dll Distributions/Windows/Libraries.64/.
     mv pgsql/bin/libpq.dll Distributions/Windows/Libraries.64/.
     mv pgsql/bin/libx*.dll Distributions/Windows/Libraries.64/.
     chmod +r,+w,-x Distributions/Windows/Libraries.64/*.dll
-    rm -f $postgresql
+    rm -f "$postgresql"
     rm -fr pgsql
 else
     echo "Cannot read $postgresql."
