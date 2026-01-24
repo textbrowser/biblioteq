2fd2c050097cab7c73665861a1f4a766509aa312
textbrowser <textbrowser@gmail.com>
2026-01-23 19:17:05 -0500

    GH #444.

--- Source/biblioteq_sqlite_create_schema.h
+++ Source/biblioteq_sqlite_create_schema.h
@@ -104,6 +104,19 @@ CREATE TABLE book_sequence						\
     value INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT			\
 );									\
 									\
+CREATE TABLE book_statistics						\
+(									\
+    author          TEXT NOT NULL,					\
+    id              VARCHAR(32),					\
+    isbn13          VARCHAR(32),					\
+    keyword         TEXT,						\
+    location        TEXT NOT NULL,					\
+    originality     TEXT,						\
+    reserved_date   TEXT NOT NULL,					\
+    target_audience TEXT,						\
+    title           TEXT NOT NULL					\
+);									\
+									\
 CREATE TABLE cd								\
 (									\
     accession_number TEXT,						\
@@ -423,6 +436,35 @@ BEGIN									\
     DELETE FROM item_borrower WHERE item_oid = old.myoid;		\
     DELETE FROM member_history WHERE item_oid = old.myoid AND		\
                 type = old.type;					\
+END;									\
+									\
+CREATE TRIGGER book_statistics_after_item_borrower_insert		\
+AFTER INSERT ON item_borrower						\
+BEGIN									\
+    INSERT INTO book_statistics						\
+    (author,								\
+     isbn13,								\
+     keyword,								\
+     location,								\
+     originality,							\
+     reserved_date,							\
+     target_audience,							\
+     title)								\
+    SELECT								\
+    book.author,							\
+    book.isbn13,							\
+    book.keyword,							\
+    book.location,							\
+    book.originality,							\
+    SUBSTR(NEW.reserved_date, 7, 4) ||					\
+    '-' ||								\
+    SUBSTR(NEW.reserved_date, 1, 2) ||					\
+    '-' ||								\
+    SUBSTR(NEW.reserved_date, 4, 2),					\
+    book.target_audience,						\
+    book.title								\
+    FROM book								\
+    WHERE book.myoid = NEW.item_oid;					\
 END;									\
 									\
 CREATE TRIGGER cd_purge_trigger AFTER DELETE ON cd			\
