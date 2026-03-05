91281298b27e5e9ec0e707d0bafe42e22df43f16
textbrowser <textbrowser@gmail.com>
2026-03-05 09:58:08 -0500

    Unique errors.

--- Tools/html2spell.bash
+++ Tools/html2spell.bash
@@ -2,4 +2,5 @@
 
 # Alexis Megas.
 
-sed 's/<b>//' $1 | sed 's/<\/b>//' | sed 's/<li>//' | sed 's/<\/li>//' | spell
+sed 's/<b>//' $1 | sed 's/<\/b>//' | sed 's/<li>//' | sed 's/<\/li>//' | \
+    spell | sort -u
